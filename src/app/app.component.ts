ee7bca63c99fdba7019141e22a3ca1ccbdc72efb
Rastosh <shivansh.rastogi19@vit.edu>
2023-05-19 00:54:06 +0530

    home page hero section draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { LanguageService } from "src/app/services/language/language.service"
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'muffeez-portfolio';
+  title = 'Shivansh -portfolio';
 
   constructor(
     private titleService: Title,
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
 
     this.languageService.initLanguage()
 
-    this.titleService.setTitle("Muffeez Khalid | MEAN Stack Developer | MERN Stack Developer | Software Engineer");
+    this.titleService.setTitle("Shivansh Rastogi | Full Stack Developer | Spring Developer | Software Engineer");
     this.metaService.addTags([
       { name: 'keywords', content: 'Frontend, MEAN Stack Developer , MERN Stack Development , Software Engineer, software, developer' },
       { name: 'description', content: 'As a software engineer with expertise in both MEAN and MERN stacks, I have a comprehensive understanding of full-stack web development. My strong foundation in JavaScript allows me to effectively work with front-end technologies such as Angular and React, as well as back-end technologies such as Node.js and Express.' },
